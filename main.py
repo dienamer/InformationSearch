6bb1b70fb7c03ddb3716c477834d617b12976bd7
aka_zz <qwe788954@163.com>
2022-12-01 00:49:13 +0800

    test2

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
-import xlrd
-data=xlrd.open_workbook('assets_20221122.xls')
-table=data.sheet_by_index(0)
+# import xlrd
+# data=xlrd.open_workbook('assets_20221122.xls')
+# table=data.sheet_by_index(0)
+#
+# row=table.nrows
+# print(row)
+#
+# city_list = [str(table.cell_value(i, 11)) for i in range(1, row)]
+# domain_list=[str(table.cell_value(i, 0)) for i in range(1, row)]
+# domain=[]
+# print(domain_list)
+# with open('url.txt','r') as f:
+#     domain=f.read().splitlines()
+# for d in domain:
+#     str1="http://"+d
+#     str2="https://"+d
+#     if  str1 in domain_list:
+#         print("域名:{0}---->指纹:{1}".format(d,city_list[domain_list.index(str1)]))
+#     if  str2 in domain_list:
+#         print("域名:{0}---->指纹:{1}".format(d,city_list[domain_list.index(str2)]))
 
-row=table.nrows
-print(row)
-
-city_list = [str(table.cell_value(i, 11)) for i in range(1, row)]
-domain_list=[str(table.cell_value(i, 0)) for i in range(1, row)]
-domain=[]
-print(domain_list)
-with open('url.txt','r') as f:
-    domain=f.read().splitlines()
-for d in domain:
-    str1="http://"+d
-    str2="https://"+d
-    if  str1 in domain_list:
-        print("域名:{0}---->指纹:{1}".format(d,city_list[domain_list.index(str1)]))
-    if  str2 in domain_list:
-        print("域名:{0}---->指纹:{1}".format(d,city_list[domain_list.index(str2)]))
+url='''173.201.192.129 
+173.201.193.240 
+72.167.218.138 
+173.201.193.97 
+68.178.252.117 
+173.201.192.158 
+97.74.135.10 
+97.74.135.143'''
+a=url.split()
+print(a)
--- 自己写的工具/ARL.py
+++ 自己写的工具/ARL.py
@@ -7,39 +7,53 @@ from alive import UrlCheck
 class ARL(Thread):
     ARL_URL_QUENE = Queue()
     ARL_ALIVE_URL = []
-    ARL_ALIVE_URL_INDEFITY=[]
-    ARL_ALIVE_IP=[]
+    ARL_ALIVE_URL_INDEFITY = []
+    ARL_ALIVE_IP = []
+    ARL_ALIVE_PORT=[]
+    ARL_ALIVE_MAP = {}
+
     def __init__(self, file_name):
         super().__init__()
         ARL_DATA = xlrd.open_workbook(file_name)
-        self.ARL_INFORMATION_MAP={}
-        ARL_INFORMATION_LIST=[]
-        for i in range(0,4):
-            ARL_TABLE=ARL_DATA.sheet_by_index(i)
-            for col in range(0,ARL_TABLE.ncols):
-                for row in range(0,ARL_TABLE.nrows):
-                    ARL_INFORMATION_LIST.append([str(ARL_TABLE.cell_value(j,col)) for j in range(1,row)])
+        self.ARL_INFORMATION_MAP = {}
 
-
-
-
+        for i in range(0, 4):
+            ARL_INFORMATION_LIST = []
+            ARL_TABLE = ARL_DATA.sheet_by_index(i)
+            for col in range(0, ARL_TABLE.ncols):
+                row = ARL_TABLE.nrows
+                ARL_INFORMATION_LIST.append([str(ARL_TABLE.cell_value(j, col)) for j in range(1, row)])
+            self.ARL_INFORMATION_MAP[i] = ARL_INFORMATION_LIST
 
     def run(self):
-        for i in range(50):
-            bdm = UrlCheck(self.ARL_URL_QUENE, self.ARL_ALIVE_URL)
-            bdm.daemon = True
-            bdm.start()
-        self.ARL_URL_QUENE.join()
+        self.GetAlive()
         self.Clean()
+        # for i in range(50):
+        #     bdm = UrlCheck(self.ARL_URL_QUENE, self.ARL_ALIVE_URL)
+        #     bdm.daemon = True
+        #     bdm.start()
+        # self.ARL_URL_QUENE.join()
+        # self.Clean()
 
+    def GetAlive(self):
+        for url in self.ARL_INFORMATION_MAP.get(3)[0]:
+            self.ARL_URL_QUENE.put(url)
+        for i in range(50):
+            Check = UrlCheck(self.ARL_URL_QUENE, self.ARL_ALIVE_URL)
+            Check.daemon = True
+            Check.start()
+        self.ARL_URL_QUENE.join()
 
     def Clean(self):
-    ##获取IP
+        ##获取对应IP
+        IP_LIST = self.ARL_INFORMATION_MAP.get(3)[3]
+        URL_LIST = self.ARL_INFORMATION_MAP.get(3)[0]
+        PORT_LIST = self.ARL_INFORMATION_MAP.get(1)[1]
         for url in self.ARL_ALIVE_URL:
-               self.ARL_ALIVE_IP.append()
-        print(self.ARL_ALIVE_URL_INDEFITY)
-    ##获取对应ip
-
-
-
-
+            self.ARL_ALIVE_IP.append(IP_LIST[URL_LIST.index(url)].split())
+        print(self.ARL_ALIVE_IP)
+    ##获取ip对应的端口
+        for list in self.ARL_ALIVE_IP:
+            for ip in list:
+                self.ARL_ALIVE_PORT.append(PORT_LIST[IP_LIST.index(ip)].split())
+        print(self.ARL_ALIVE_PORT)
